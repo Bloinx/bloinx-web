8ca650ae8c5754fb4e2c0d6cd4e4de139aa1aa2c
ArturVargas <arturvargas9226@gmail.com>
2020-07-19 20:04:36 -0500

    create user list

--- contracts/Groups.sol
+++ contracts/Groups.sol
@@ -1,23 +1,46 @@
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity >=0.4.22 <0.7.0;
+pragma experimental ABIEncoderV2;
 
 contract Groups {
-  struct User {
-    uint userId;
-    string userName;
-    address payable founds;
-  }
+    struct User {
+        uint256 userId;
+        string userName;
+        address payable founds;
+        bool fee;
+    }
+    
+    struct Room {
+        uint256 roomId;
+        User user;
+    }
+    
+    mapping(uint => User) public users;
+    mapping(uint => Room) public rooms;
+    uint roomCounter = 1;
+    uint usersCounter = 0;
+    
+    User[] usersList;
+    Room[3][] roomsList;
+    
+    address public admin;
+    
+    constructor() public {
+        admin = msg.sender;
+    }
+    
+    function registerUser(string memory _userName) public {
+        if(usersCounter >= 2) {
+            revert("El grupo esta completo");
+        }
+        usersCounter ++;
+        users[usersCounter] = User(usersCounter, _userName, msg.sender, false);
+        usersList.push(users[usersCounter]);
+    }
 
-  address public admin;
-
-  constructor() public {
-    admin = msg.sender;
-  }
-
-  function createGroup() public {
-    // Se creara una posicion en el arreglo y ese sera un grupo.
-  }
-
-  function addUsersToGroup() public {
-    // Se tomaran X cantidad de usuarios para añadirlos al grupo que se esta formando.
-  }
+    function payFee() public payable {
+     // here is where user have to pay fee for guarantee
+    }
+    //function createRoom() public {
+       //rooms[roomCounter] = Room(roomCounter, users[1].userName)
+    //}
 }
