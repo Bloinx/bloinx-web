c9ebbfe105d3d4320fe0b2e8436cb60c1d919e86
AndreaV <andyvargtz@gmail.com>
2020-07-21 00:50:44 -0500

    add Mejoras al contrato

--- contracts/Groups.sol
+++ contracts/Groups.sol
@@ -7,15 +7,15 @@ contract Groups {
         setup,      //receive the initial fees
         save,     //receive payments for stage number 1
         pay,      //send savings to user in 1rst number
-        finished //all users Withdraw their saving
+        finished  //all users Withdrew their saving
     }
     
     struct User {
         uint256 userId;
         string userName;
-        address payable founds;
-        bool fee;
-        bool saveRound;
+        address payable userAddr;
+        bool cashInFlag;
+        bool saveAmountFlag;
     }
     
     struct Room {
@@ -27,12 +27,14 @@ contract Groups {
     mapping(uint => Room) public rooms;
     uint roomCounter = 1;
     uint usersCounter = 0;
-    uint feeSize;
-    uint saveSize; //Monto de ahorro periodicamente
+    uint cashIn;
+    uint saveAmount; //Monto de ahorro periodicamente
     uint groupSize;
-    uint paymtCounterRound = 0;  //Cantidad de pagos que se realizan por stage
-    uint ronda = 0;   //Sirve para saber el usuario en turno
-    uint amount = saveSize * groupSize;  //Monto del ahorro logrado en la tanda
+    uint CashInPayeesCount = 0;
+    uint saveAmountPayeesCount = 0;  //Cantidad de pagos que se realizan por stage
+    uint cycle = 0;   //Sirve para saber el usuario en turno
+    uint totalSaveAmount = 0;  //Monto del ahorro logrado en la tanda
+    uint totalCashIn = 0;
 
     Stages stage;
 
@@ -42,60 +44,74 @@ contract Groups {
     address payable public admin;
 
     modifier isUsersTurn() {
-        require(msg.sender == usersList[ronda].founds, "Debes esperar tu turno para retirar");
+        require(msg.sender == usersList[cycle].userAddr, "Debes esperar tu turno para retirar");
         _;
     }
 
-    constructor(uint _fee, uint _saveAmt, uint8 _groupSize) public {
+    modifier isRegisteredUser() {
+        require(msg.sender == users[msg.sender].userAddr, "Usuario no registrado");
+        _;
+    }
+
+    constructor(uint _cashIn, uint _saveAmount, uint8 _groupSize) public {
         admin = msg.sender;
-        feeSize = _fee;
-        saveSize = _saveAmt;
+        cashIn = _cashIn;
+        saveAmount = _saveAmount;
         groupSize = _groupSize;
         stage = Stages.setup;
     }
 
     function registerUser(string memory _userName) public {
         require(stage == Stages.setup, "La tanda ya comenzo y no acepta nuevos usuarios");
-        if(usersCounter >= groupSize){
-            revert("El grupo esta completo");  // ***?? diferencia con require(usersCounter >= groupSize, "El grupo esta completo");***
-        }
-        usersCounter ++;
+        require(usersCounter < groupSize, "El grupo esta completo");
+        usersCounter++;
         users[msg.sender] = User(usersCounter, _userName, msg.sender, false, false);
         usersList.push(users[msg.sender]);
     }
 
-    function payFee() public payable {
+    function payFee() public isRegisteredUser payable {
         require(stage == Stages.setup, "Todos han pagado el fee exitosamente");
-        require(msg.value >= feeSize, 'Fondos Insuficientes');
-        admin.transfer(msg.value);
-        users[msg.sender].fee = true;
-        paymtCounterRound++;
-        if (paymtCounterRound == groupSize){  // ***Aquí tenemos que cada usuario haya realizado solo un pago***
-            paymtCounterRound = 0; // ***podemos cambiarlo iterando sobre el arreglo para ver que todos los .fee sean true, pero creo que es mas costoso***
+        require(users[msg.sender].cashInFlag == false, "Ya tenemos regisrado tu CashIn");
+        require(msg.value >= cashIn, 'Fondos Insuficientes');
+        totalCashIn = totalCashIn + msg.value;
+        users[msg.sender].cashInFlag = true;
+        CashInPayeesCount++;
+        if (CashInPayeesCount == groupSize){
+            admin.transfer(totalCashIn);
+            CashInPayeesCount = 0;
             stage = Stages.save;
         }
     }
 
 
-    function payRound() public payable {
+    function payRound() public isRegisteredUser payable {
         require(stage == Stages.save, "Espera una nueva ronda de ahorro");
-        require(msg.value >= saveSize, 'Fondos Insuficientes');
-        admin.transfer(msg.value);
-        users[msg.sender].saveRound = true;
-        paymtCounterRound ++;
-        if (paymtCounterRound == groupSize){
-            paymtCounterRound = 0;
+        require(users[msg.sender].saveAmountFlag == false, "Ya ahorraste esta ronda");
+        require(msg.value >= cashIn, 'Fondos Insuficientes');
+        users[msg.sender].saveAmountFlag = true;
+        saveAmountPayeesCount++;
+        totalSaveAmount = totalSaveAmount + msg.value;
+        if (saveAmountPayeesCount == groupSize){
+            saveAmountPayeesCount = 0;
             stage = Stages.pay;            //La tanda puede pagarse si todos ahorraron en la ronda
         }
     }
     
-    function WithdrawRound() public isUsersTurn payable {
+    function WithdrawRound() public isRegisteredUser isUsersTurn payable {
         require(stage == Stages.pay, "Espera a que tengamos el monto de tu ahorro");     //Se debe estar en fase de pago
         stage = Stages.save;
-        usersList[ronda].founds.transfer(amount);
-        ronda++;
-        if(ronda > groupSize){
+        usersList[cycle].userAddr.transfer(totalSaveAmount);
+        cycle++;
+        stage = Stages.save;
+        newRound();
+        if(cycle > groupSize){
             stage = Stages.finished;
+        }
+    }
+
+    function newRound() internal {          //*********Esta funcion no sirve bien*** Revisar
+        for(uint8 i; i<groupSize; i++ ){
+            usersList[i].saveAmountFlag = false;
         }
     }
 
