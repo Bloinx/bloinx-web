fb631262c5a757628657b9bb38d5fad9fbf6f399
nitaxmx <66643244+nitaxmx@users.noreply.github.com>
2020-08-30 00:32:25 -0500

    Update GroupsTime.sol
    Se agrega modificarod para manejo de tiempo independiente de las funciones

--- contracts/GroupsTime.sol
+++ contracts/GroupsTime.sol
@@ -41,14 +41,14 @@ contract Groups {
     uint saveAmountPayeesCount = 0;  //Payments done in round counter
     uint cycle = 1;   //Current cycle/round in the saving circle
     uint public creationTime;
-    uint public tandaTime;
+    //uint public tandaTime;
 
     uint totalSaveAmount = 0;  //Collective saving on the round
     uint totalCashIn = 0;
 
     Stages public stage;
 
-    address[] public addressOrderList;
+    address[] addressOrderList;
     //Room[] roomsList;
 
     modifier isUsersTurn() {    //Verifies if it is the users round to widraw
@@ -61,25 +61,23 @@ contract Groups {
         _;
     }
     
-    modifier stageSetupTime() {    //Verifies if it is the users round to widraw
-        require(now <= creationTime + 2 minutes, 'Se termino el tiempo de registro');
-        _;
-    }
-    
     modifier isPayAmountCorrect() {    //Verifies if it is the users round to widraw
         require(msg.value >= cashIn, 'Fondos Insuficientes');
         _;
     }
     
-    //modifier timedTransitions() {
-    //    if (stage == Stages.setup && now >= creationTime + 2 minutes)
-    //        stage = Stages.save;
-        //if (stage == Stages.save && now >= tandaTime + 3 minutes)
-        //    Stages.finished;
-        //if (stage == Stages.finished && now >= tandaTime + 4 minutes)
-        //    nextStage();
-    //    _;
-    //}
+    modifier atStage(Stages _stage) {
+        require(stage == _stage);
+        _;
+    }
+    
+    modifier timedTransitions() {
+        if (stage == Stages.setup && now >= creationTime + 2 minutes)
+            stage = Stages.save;
+        if (stage == Stages.save && now >= creationTime + 2 minutes + groupSize*60)
+            stage=Stages.finished;
+        _;
+    }
 
     constructor(uint _cashIn, uint _saveAmount, uint8 _groupSize) public {
         admin = msg.sender;
@@ -87,19 +85,17 @@ contract Groups {
         saveAmount = _saveAmount;
         groupSize = _groupSize;
         stage = Stages.setup;
-        tandaTime=now;
+        creationTime=now;
     }
 
-    function registerUser(string memory _userName) public stageSetupTime {
-        require(stage == Stages.setup, "La tanda ya comenzo y no acepta nuevos usuarios");  //The saving circle has started and doesn't accept new users
+    function registerUser(string memory _userName) public timedTransitions atStage(Stages.setup){
         require(usersCounter < groupSize, "El grupo esta completo");    //the saving circle is full
         usersCounter++;
         users[msg.sender] = User(usersCounter, _userName, msg.sender, false, false, true);
         addressOrderList.push(msg.sender);  //store user
     }
 
-    function payCashIn() public stageSetupTime isRegisteredUser isPayAmountCorrect payable {       //Receive the comitment payment
-        require(stage == Stages.setup, "Todos han pagado su entrada exitosamente");
+    function payCashIn() public timedTransitions isRegisteredUser isPayAmountCorrect atStage(Stages.setup) payable {       //Receive the comitment payment
         require(users[msg.sender].cashInFlag == false, "Ya tenemos regisrado tu CashIn"); //you have payed the cash in
         totalCashIn = totalCashIn + msg.value;
         users[msg.sender].cashInFlag = true;
@@ -107,16 +103,13 @@ contract Groups {
         if (CashInPayeesCount == groupSize){
             admin.transfer(totalCashIn);
             CashInPayeesCount = 0;
-            //tandaTime=now;
-            stage = Stages.save;
         }
     }
 
 
-    function payRound() public isRegisteredUser isPayAmountCorrect payable {    //users make the payment for the cycle
-        require(stage == Stages.save, "Espera una nueva ronda de ahorro");  //wait for a new round
+    function payRound() public timedTransitions isRegisteredUser isPayAmountCorrect atStage(Stages.save) payable {    //users make the payment for the cycle
         require(users[msg.sender].saveAmountFlag == false, "Ya ahorraste esta ronda");  //you have already saved this round
-        require(now <= tandaTime + 1 minutes, 'Pago tardio');
+        require(now <= creationTime + 2 minutes + cycle*60 , 'Pago tardio');
         users[msg.sender].saveAmountFlag = true;
         saveAmountPayeesCount++;
         totalSaveAmount = totalSaveAmount + msg.value;
@@ -125,17 +118,16 @@ contract Groups {
         }
     }
 
-    function WithdrawRound() payable isRegisteredUser isUsersTurn public {   //User assigned to the round can widraw
+    function WithdrawRound() payable timedTransitions isRegisteredUser isUsersTurn atStage(Stages.save) public {   //User assigned to the round can widraw
         require(totalSaveAmount==groupSize*saveAmount, "Espera a que tengamos el monto de tu ahorro");     //Se debe estar en fase de pago
         address addressUserInTurn = addressOrderList[cycle-1];
         users[addressUserInTurn].userAddr.transfer(totalSaveAmount);
         cycle++;
-        tandaTime=now;
         newRound();
-        if(cycle > groupSize){
+        /*if(cycle > groupSize){
             stage = Stages.finished;
 
-        }
+        }*/
     }
 
 
@@ -148,8 +140,7 @@ contract Groups {
     }
 
 
-    function withdrawCashIn() payable isPayAmountCorrect public {  //When all the rounds are done the admin sends the cash in to the users
-        require(stage == Stages.finished, "Debes esperar a que termine el circulo de ahorro");//wait for the saving circle to end
+    function withdrawCashIn() payable timedTransitions isPayAmountCorrect public atStage(Stages.finished){  //When all the rounds are done the admin sends the cash in to the users
         for(uint8 i = 0; i<groupSize; i++){
             address useraddress = addressOrderList[i];
             users[useraddress].cashInFlag = false;
@@ -163,5 +154,6 @@ contract Groups {
         stage = Stages.setup;
         totalSaveAmount = 0;
         cycle=1;
+        creationTime=now;
     }
 }
