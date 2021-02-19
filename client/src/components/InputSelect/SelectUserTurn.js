0deeaee2305f0b6e0ed831160b90f6d278b8a52e
ArturVargas <arturvargas9226@gmail.com>
2021-02-19 08:38:01 -0600

    add turn user option

--- client/src/components/InputSelect/SelectUserTurn.js
+++ client/src/components/InputSelect/SelectUserTurn.js
@@ -9,6 +9,7 @@ export function SelectUserTurn() {
       <option value="1">1</option>
       <option value="2">2</option>
       <option value="3">3</option>
+      <option value="4">4</option>
     </select>
   );
 }
