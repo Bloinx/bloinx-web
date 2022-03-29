fad084cfd8cefa6fa9bb653fba5fbbdcc86cc83c
Karen Tapia <75274720+Kar3n1na@users.noreply.github.com>
2022-03-28 19:48:30 -0600

    Revert "fix valora integration (#97)"
    This reverts commit 25ab9571e45da493177adb056e2b916f874f58e0.

--- src/api/getRounds.js
+++ src/api/getRounds.js
@@ -30,9 +30,9 @@ const getRounds = async ({ userId, walletAddress }) => {
 
   return new Promise((resolve) => {
     const rounds = [];
-    let i = 1;
+    let i = 0;
 
-    querySnapshot.docs.forEach(async (doc) => {
+    querySnapshot.forEach(async (doc) => {
       const data = doc.data();
       const sg = await config(data.contract);
       console.log(sg);
@@ -132,7 +132,7 @@ const getRounds = async ({ userId, walletAddress }) => {
       };
       rounds.push(roundData);
 
-      if (i <= querySnapshot.size) {
+      if (i === querySnapshot.size - 1) {
         console.log("Rondas::", rounds);
         resolve(rounds.sort());
       } else {
